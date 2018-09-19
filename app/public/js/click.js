8334d8e7f38d92b33efd9f14937ea25a6bbbe82a
Erin Rizal <erinrizal@Erins-MacBook-Pro.local>
2018-09-19 17:50:49 -0400

    looking awesome

--- app/public/js/click.js
+++ app/public/js/click.js
@@ -1,31 +1,37 @@
 
 $(document).ready(function() {
   $('.parallax').parallax();
+  $('.sidenav').sidenav();
   var topics = [
-    "cats",
-    "kittens",
+    "kitten",
     "dogs",
-    "puppies",
+    "puppy",
     "capybara",
     "hamster",
     "chinchilla",
     "rabbit",
     "alpaca",
-    "baby tiger"
+    "baby tiger",
+    "pallas cat",
+    "dolphin",
+    "trending"
   ];
+  
      $.each(topics, function(i, val) {
+       
           var button = $("<button>");
           button.addClass("btn");
           button.text(val);
           button.attr("id", "topic_" + i + "  ");
           $("#buttonWell").append(button);
     })
-  $("#buttonWell").on("click", 'button', function() {
 
+  $("#buttonWell").on("click", 'button', function() {
     var animal = $(this).text();
     var queryURL ="https://api.giphy.com/v1/gifs/search?q=" + animal + 
     "&api_key=IRB5UHpnFotXqJl0k2m6bqbTX5PAdV67";
-    
+    console.log(queryURL)
+
     $.ajax({
           url: queryURL,
           method: "GET",
@@ -36,15 +42,26 @@ $(document).ready(function() {
                 console.log("error", arguments[2])
             }
           }).done(function(response) {
-            for (var k = 0; k < 10; k++) {
-            // Saving the image_original_url property
+            for (var k = 0; k < 15; k++) {
             var results = response.data;
-           
             var animated = results[k].images.fixed_height.url;
             var still = results[k].images.fixed_height_still.url;
-       
+            var rating =   results[k].rating;
+              if (rating === "r") {
+                var removed = results.splice(data);
+                console.log(removed)
+              }
+            var time =   results[k].import_datetime;
+            var title =   results[k].title;
+            console.log(title);
+            var giphyUrl =   results[k].url;
+            var origUrl = results[k].source;
+            var gLink = '<a href="' + giphyUrl + '" target="_blank">Giphy</a>';
+            var oUrl = '<a href="' + origUrl + '" target="_blank">Source</a>';
+            var p = $("<div class='descript'>").html( "<p class='titleCard'>"+ title + "</p>" + "<b>Added at:</b> " + time + 
+            "<br>" + "<b>Found at:</b> " + gLink + " | " + oUrl + "<br>" + "<b>Rating:</b> " + rating);
             // Creating and storing an image tag
-            var cardHold = $("<div class=\"cardA\">");
+            var cardHold = $("<div class=\"cardA animated fadeInDown delay-2s \">");
             var gifImage = $("<img class='imgdis'>");
             gifImage.attr("src", still);
             gifImage.attr("data-still", still);
@@ -52,19 +69,21 @@ $(document).ready(function() {
             gifImage.attr("data-state", "still");
             gifImage.attr("src", animated);
             gifImage.attr("alt", "image");
-            cardHold.append(gifImage);
-            $("#images").append(cardHold);
-              // var answer = response.volumes;
-              // console.log("answer is: " + volumes[k])  
-              // var rating = $("<p>").text("Rating: " + answer[k].rating);
-              // var imageGif = $("<img>").text(rating);
-              // imageGif.attr("src", answer[k].images.fixed_height.url);
-              // $("#images").prepend(imageGif).before(rating);
-              // imageGif.css("padding", "8px");
-              // imageGif.addClass("gif");
-              // var animateGif= imageGif.attr("data-animate", answer[k].images.fixed_height.url);
-              // var stillGif= imageGif.attr("data-still", answer[k].images.fixed_height_still.url);
+            
+            cardHold.append(gifImage, p);
+            $("#animals").prepend(cardHold)
              }
+             $('.imgdis').on("click", ".card", function() {
+               alert("test")
+             });
         });
+  });
+  $("#new-tag").on("click", function(event) {
+    event.preventDefault();
+    var newAnimal = $("input").eq(0).val();
+
+    if (newAnimal.length > 2) {
+      topics.push(newAnimal);
+    }
   });
 });
--- app/public/js/old.js
+++ app/public/js/old.js
@@ -1,88 +1,89 @@
+
 $(document).ready(function() {
   $('.parallax').parallax();
+  $('.sidenav').sidenav();
   var topics = [
-    "cats",
-    "kittens",
+    "kitten",
     "dogs",
-    "puppies",
+    "puppy",
     "capybara",
     "hamster",
     "chinchilla",
     "rabbit",
     "alpaca",
-    "baby tiger"
+    "baby tiger",
+    "pallas cat",
+    "dolphin",
+    "trending"
   ];
-
+  
      $.each(topics, function(i, val) {
+       
           var button = $("<button>");
           button.addClass("btn");
           button.text(val);
           button.attr("id", "topic_" + i + "  ");
-          button.css("margin", "8px");
-          $(".card").append(button);
+          $("#buttonWell").append(button);
     })
 
+  $("#buttonWell").on("click", 'button', function() {
+    var animal = $(this).text();
+    var queryURL ="https://api.giphy.com/v1/gifs/search?q=" + animal + 
+    "&api_key=IRB5UHpnFotXqJl0k2m6bqbTX5PAdV67";
+    console.log(queryURL)
 
- $("#buttonWell").on("click", 'button', function() {
+    $.ajax({
+          url: queryURL,
+          method: "GET",
+            success: function() {
+                console.log("success!")
+            },
+            error: function() {
+                console.log("error", arguments[2])
+            }
+          }).done(function(response) {
+            for (var k = 0; k < 15; k++) {
+            var results = response.data;
+            var animated = results[k].images.fixed_height.url;
+            var still = results[k].images.fixed_height_still.url;
+            var rating =   results[k].rating;
+              if (rating === "r") {
+                var removed = results.splice(data);
+                console.log(removed)
+              }
+            var time =   results[k].import_datetime;
+            var title =   results[k].title;
+            console.log(title);
+            var giphyUrl =   results[k].url;
+            var origUrl = results[k].source;
+            var gLink = '<a href="' + giphyUrl + '" target="_blank">Giphy</a>';
+            var oUrl = '<a href="' + origUrl + '" target="_blank">Source</a>';
+            var p = $("<div class='descript'>").html( "<p class='titleCard'>"+ title + "</p>" + "<b>Added at:</b> " + time + 
+            "<br>" + "<b>Found at:</b> " + gLink + " | " + oUrl + "<br>" + "<b>Rating:</b> " + rating);
+            // Creating and storing an image tag
+            var cardHold = $("<div class=\"cardA animated fadeInDown delay-2s\">");
+            var gifImage = $("<img class='imgdis'>");
+            gifImage.attr("src", still);
+            gifImage.attr("data-still", still);
+            gifImage.attr("data-animate", animated);
+            gifImage.attr("data-state", "still");
+            gifImage.attr("src", animated);
+            gifImage.attr("alt", "image");
+            
+            cardHold.append(gifImage, p);
+            $("#animals").prepend(cardHold)
+             }
+             $('.imgdis').on("click", ".card", function() {
+               alert("test")
+             });
+        });
+  });
+  $("#new-tag").on("click", function(event) {
+    event.preventDefault();
+    var newAnimal = $("input").eq(0).val();
 
-  var animal = $(this).text()
-
-  console.log("this is the animal" + animal + "_____________________")
-
-  var queryURL ="https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=IRB5UHpnFotXqJl0k2m6bqbTX5PAdV67";
-  var $ = require("jquery")(window);
-  
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).done(function(response) {
-
-      for (var k = 0; k < 10; k++) {
-      var answer = response.volumes;
-        console.log("answer is: " + volumes[k])
-      
-        var rating = $("<p>").text("Rating: " + answer[k].rating);
-        var imageGif = $("<img>").text(rating);
-        imageGif.attr("src", answer[k].images.fixed_height.url);
-        $("#images").prepend(imageGif).before(rating);
-        imageGif.css("padding", "8px");
-        imageGif.addClass("gif");
-        var animateGif= imageGif.attr("data-animate", answer[k].images.fixed_height.url);
-        var stillGif= imageGif.attr("data-still", answer[k].images.fixed_height_still.url);
-       }
-
-// PAUSE-START
-     $(".gif").on("click", function() { 
-     
-        var state = $(this).attr('data-state');
-        var animate = $(this).attr('data-animate');
-        var still= $(this).attr('data-still');
-
-        // =============================================
-
-        // STEP THREE: Check if the variable state is equal to 'still',
-        if (state === 'still'){ //== would be evaluating value
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-        
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
-    });
-
-
-      // submit button is clicked
-      $(".btn btn-default").on("click", function(event) {
-        event.preventDefault();
-        // This grabs the input from the textbox
-        var newTag = $("#input-id").text(JSON.stringify(response));
-        newTag.push(topics[i]);
-        console.log(newTag);
-      });
-
-      $(document).on("click", "<img>", response);
-    
+    if (newAnimal.length > 2) {
+      topics.push(newAnimal);
+    }
+  });
 });
-});
- });
