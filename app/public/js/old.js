06c3b2ac26fe08eeadf1c453bb61def03374d39f
Erin Rizal <erinrizal@Erins-MacBook-Pro.local>
2018-09-20 13:50:56 -0400

    more responsiveness

--- app/public/js/old.js
+++ app/public/js/old.js
@@ -16,22 +16,36 @@ $(document).ready(function() {
     "dolphin",
     "trending"
   ];
-  
-     $.each(topics, function(i, val) {
-       
-          var button = $("<button>");
-          button.addClass("btn");
-          button.text(val);
-          button.attr("id", "topic_" + i + "  ");
-          $("#buttonWell").append(button);
-    })
+  // $("#add-animal").on("click", function(event) {
+  //   event.preventDefault();
+  //   var newAnimal = $("input").eq(0).val();
+  //   if (newAnimal.length > 2) {
+  //     topics.push(newAnimal);
+  //     alert(topics)
+  //   }
+  // });
+  function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+    $(areaToAddTo).empty();
+    for (var i = 0; i < arrayToUse.length; i++) {
+      var button = $("<button>");
+      button.addClass(classToAdd);
+      button.attr("data-type", arrayToUse[i]);
+      button.text(arrayToUse[i]);
+      $(areaToAddTo).append(button);
+    }
+  }
+    //  $.each(topics, function(i, val) {
+    //       var button = $("<button>");
+    //       button.addClass("btn");
+    //       button.text(val);
+    //       button.attr("id", "topic_" + i + "  ");
+    //       $("#buttonWell").append(button);
+    // })
 
-  $("#buttonWell").on("click", 'button', function() {
+  $("#animal-buttons").on("click", 'button', function() {
     var animal = $(this).text();
     var queryURL ="https://api.giphy.com/v1/gifs/search?q=" + animal + 
     "&api_key=IRB5UHpnFotXqJl0k2m6bqbTX5PAdV67";
-    console.log(queryURL)
-
     $.ajax({
           url: queryURL,
           method: "GET",
@@ -42,27 +56,22 @@ $(document).ready(function() {
                 console.log("error", arguments[2])
             }
           }).done(function(response) {
-            for (var k = 0; k < 15; k++) {
             var results = response.data;
+            for (var k = 0; k < results.length; k++) {
             var animated = results[k].images.fixed_height.url;
             var still = results[k].images.fixed_height_still.url;
             var rating =   results[k].rating;
-              if (rating === "r") {
-                var removed = results.splice(data);
-                console.log(removed)
-              }
-            var time =   results[k].import_datetime;
-            var title =   results[k].title;
-            console.log(title);
-            var giphyUrl =   results[k].url;
-            var origUrl = results[k].source;
-            var gLink = '<a href="' + giphyUrl + '" target="_blank">Giphy</a>';
-            var oUrl = '<a href="' + origUrl + '" target="_blank">Source</a>';
-            var p = $("<div class='descript'>").html( "<p class='titleCard'>"+ title + "</p>" + "<b>Added at:</b> " + time + 
+             if (rating === "r") {
+             var removed = results.splice(data);
+            }
+            var gLink = '<a href="' + results[k].url + '" target="_blank">Giphy</a>';
+            var oUrl = '<a href="' + results[k].source + '" target="_blank">Source</a>';
+            var p = $("<div class='descript'>").html( "<p class='titleCard'>"+ results[k].title + "</p>" + "<b>Added at:</b> " + 
+            results[k].import_datetime + 
             "<br>" + "<b>Found at:</b> " + gLink + " | " + oUrl + "<br>" + "<b>Rating:</b> " + rating);
             // Creating and storing an image tag
-            var cardHold = $("<div class=\"cardA animated fadeInDown delay-2s\">");
-            var gifImage = $("<img class='imgdis'>");
+            var cardHold = $("<div class=\"cardA animated fadeInDown delay-3s \">");
+            var gifImage = $("<img class='imgdis animated fadeIn'>");
             gifImage.attr("src", still);
             gifImage.attr("data-still", still);
             gifImage.attr("data-animate", animated);
@@ -73,17 +82,7 @@ $(document).ready(function() {
             cardHold.append(gifImage, p);
             $("#animals").prepend(cardHold)
              }
-             $('.imgdis').on("click", ".card", function() {
-               alert("test")
-             });
         });
   });
-  $("#new-tag").on("click", function(event) {
-    event.preventDefault();
-    var newAnimal = $("input").eq(0).val();
 
-    if (newAnimal.length > 2) {
-      topics.push(newAnimal);
-    }
-  });
 });
